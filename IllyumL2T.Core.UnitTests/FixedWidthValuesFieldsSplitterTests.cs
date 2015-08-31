7992ee954a4cf3cc973e872dd2eb4c3590c8a785
Marco Dorantes <marcodorantes@hotmail.com>
2015-08-31 06:42:03 -0500

    Addded FixedWidthSplit_InvalidInput test.

---
+++ IllyumL2T.Core.UnitTests/FixedWidthValuesFieldsSplitterTests.cs
@@ -0,0 +1,25 @@
+﻿using IllyumL2T.Core.FieldsSplit.FieldsSplit;
+using IllyumL2T.Core.FieldsSplit.UnitTests.Classes_for_Testing;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace IllyumL2T.Core.FieldsSplit.UnitTests
+{
+  [TestClass]
+  public class FixedWidthValuesFieldsSplitterTests
+  {
+    [TestMethod, TestCategory("FixedWidth"), ExpectedException(typeof(ArgumentNullException))]
+    public void FixedWidthSplit_InvalidInput()
+    {
+      // Arrange
+      string line = null;
+
+      // Act
+      var splitter = new FixedWidthValuesFieldsSplitter<Record>();
+      string[] values = splitter.Split(line);
+
+      // Assert
+      Assert.Fail();
+    }
+  }
+}
