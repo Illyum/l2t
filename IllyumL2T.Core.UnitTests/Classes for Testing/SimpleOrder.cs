5fcb7dd71f765b192db319be5973982e985f9cca
Marco Dorantes <marcodorantes@hotmail.com>
2016-06-18 13:19:07 -0500

    Added PositionalValuesFieldsSplitter.cs. First take. Now the failing test pass, initially.

--- IllyumL2T.Core.UnitTests/Classes for Testing/SimpleOrder.cs
+++ IllyumL2T.Core.UnitTests/Classes for Testing/SimpleOrder.cs
@@ -1,19 +1,42 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace IllyumL2T.Core.FieldsSplit.UnitTests
 {
-  /// <summary>
-  /// Application-level objects serialized as positional/fixed-width text line.
-  /// </summary>
   class SimpleOrder
   {
-    [IllyumL2T.Core.ParseBehavior(Length = 5)]  public short OrderId { get; set; }
-    [IllyumL2T.Core.ParseBehavior(Length = 9)]  public decimal Freight { get; set; }
-    [IllyumL2T.Core.ParseBehavior(Length = 50)] public string ShipAddress { get; set; }
-    [IllyumL2T.Core.ParseBehavior(Length = 10)] public DateTime DeliveryDate { get; set; }
+    [IllyumL2T.Core.ParseBehavior(Length = 5)]
+    public short OrderId { get; set; }
+
+    [IllyumL2T.Core.ParseBehavior(Length = 9, NumberStyle = NumberStyles.AllowDecimalPoint)]
+    public decimal Freight { get; set; }
+
+    [IllyumL2T.Core.ParseBehavior(Length = 50)]
+    public string ShipAddress { get; set; }
+
+    [IllyumL2T.Core.ParseBehavior(Length = 10, DateTimeFormat = "d/M/yyyy")]
+    public DateTime DeliveryDate { get; set; }
+
+    public override bool Equals(object other)
+    {
+      if (other is SimpleOrder)
+      {
+        return ((SimpleOrder)other).GetHashCode() == this.GetHashCode();
+      }
+
+      return false;
+    }
+
+    public override int GetHashCode()
+    {
+      return OrderId.GetHashCode() +
+             Freight.GetHashCode() +
+             ShipAddress.GetHashCode() +
+             DeliveryDate.GetHashCode();
+    }
   }
 }
---
+++ IllyumL2T.Core/FieldsSplit/PositionalValuesFieldsSplitter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IllyumL2T.Core.FieldsSplit
+{
+  public class PositionalValuesFieldsSplitter<T> : IllyumL2T.Core.FieldsSplit.FieldsSplitterBase<T> where T : class, new()
+  {
+    public override string[] Split(string line)
+    {
+      var result = new List<string>();
+
+      int index = 0;
+      foreach (var fieldParser in FieldParsers)
+      {
+        int field_length = fieldParser.ParseBehavior.Length;
+        //result.Add(line.Substring(index, field_length));
+        result.Add(line.Substring(index, field_length).Trim());
+        index += field_length;
+      }
+
+      return result.ToArray();
+    }
+  }
+}
