5495ab9ef2e3bd7224701b117e6bff9ad3baafdb
Marco Dorantes <marcodorantes@hotmail.com>
2016-06-18 08:20:30 -0500

    Added the following five points: 1. PositionalValuesFileParserTests test class. 2. PositionalValuesFileParserTests.ParseFileTest failing test case. 3. ParseBehaviorAttribute.Length property. 4. PositionalValuesFileParser class. 5. SimpleOrder class for testing.

---
+++ IllyumL2T.Core.UnitTests/Classes for Testing/SimpleOrder.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IllyumL2T.Core.FieldsSplit.UnitTests
+{
+  /// <summary>
+  /// Application-level objects serialized as positional/fixed-width text line.
+  /// </summary>
+  class SimpleOrder
+  {
+    [IllyumL2T.Core.ParseBehavior(Length = 5)]  public short OrderId { get; set; }
+    [IllyumL2T.Core.ParseBehavior(Length = 9)]  public decimal Freight { get; set; }
+    [IllyumL2T.Core.ParseBehavior(Length = 50)] public string ShipAddress { get; set; }
+    [IllyumL2T.Core.ParseBehavior(Length = 10)] public DateTime DeliveryDate { get; set; }
+  }
+}
---
+++ IllyumL2T.Core/Parse/PositionalValuesFileParser.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace IllyumL2T.Core.Parse
+{
+  public class PositionalValuesFileParser<T> where T : class, new()
+  {
+    public IEnumerable<ParseResult<T>> Read(StreamReader reader, bool includeHeaders)
+    {
+      return null;
+    }
+  }
+}
