2b3fa900d5c1118bbcf20d44b8944ef758e42929
Marco Dorantes <marcodorantes@hotmail.com>
2016-07-02 08:48:16 -0500

    Added PositionalValuesFileParserTests.ParseFileWithErrorsTest test method as a similar test for the related delimited case.

--- IllyumL2T.Core.UnitTests/PositionalValuesFileParserTests.cs
+++ IllyumL2T.Core.UnitTests/PositionalValuesFileParserTests.cs
@@ -43,6 +43,14 @@ namespace IllyumL2T.Core.FieldsSplit.UnitTests
                            order.DeliveryDate);
         }
       }
+
+      var ordersWithErrorsFilePath = Path.Combine(context.DeploymentDirectory, "FixedWidthOrdersWithErrors.csv");
+      using (var writer = new StreamWriter(ordersWithErrorsFilePath))
+      {
+        writer.WriteLine("{0,5}{1,9:#0.00}{2,-50}{3,10:dd/MM/yyyy}", "A100A", "____", "ShipNowhere", "99/99/9999");  // 3 parsing errors
+        writer.WriteLine("{0,5}{1,9:#0.00}{2,-50}{3,10:dd/MM/yyyy}", "10000", "(89)", "ShipNowhere", "InvalidDate"); // 2 parsing errors
+        writer.WriteLine("{0,5}{1,9:#0.00}{2,-50}{3,10:dd/MM/yyyy}", "A100A", "####", "ShipNowhere", "99/99/9999");  // 3 parsing errors
+      }
     }
 
     public TestContext TestContext { get; set; }
@@ -61,6 +69,44 @@ namespace IllyumL2T.Core.FieldsSplit.UnitTests
 
         // Assert
         Assert.IsTrue(_orders.SequenceEqual(parseResults.Select(parseResult => parseResult.Instance)));
+      }
+    }
+
+    [TestMethod]
+    public void ParseFileWithErrorsTest()
+    {
+      // Arrange
+      var ordersWithErrorsFilePath = Path.Combine(TestContext.DeploymentDirectory, "FixedWidthOrdersWithErrors.csv");
+      using (var reader = new StreamReader(ordersWithErrorsFilePath))
+      {
+        var fileParser = new PositionalValuesFileParser<SimpleOrder>();
+
+        // Act
+        var parseResults = fileParser.Read(reader, includeHeaders: false);
+
+        // Assert
+        var actualLinesWithError = default(int);
+        var actualFieldsWithError = default(int);
+
+        foreach (var parseResult in parseResults)
+        {
+          Assert.IsNull(parseResult.Instance);
+
+          if (parseResult.Errors.Any())
+          {
+            actualLinesWithError++;
+            actualFieldsWithError += parseResult.Errors.Count();
+          }
+        }
+
+        // In the FixedWidthOrdersWithErrors.csv file created when the test class is initialized
+        // There are three lines with errors so the logger must be called three times
+        var expectedLinesWithErrors = 3;
+        Assert.AreEqual<int>(expectedLinesWithErrors, actualLinesWithError);
+
+        // And there must be eight errors in the fields being parsed
+        var expectedErrorsInFields = 8;
+        Assert.AreEqual<int>(expectedErrorsInFields, actualFieldsWithError);
       }
     }
   }
