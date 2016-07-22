316e3357a007757deaa190884a12336c0ca21436
Francisco Javier Banos Lemoine <franl@illyum.com>
2016-07-21 19:18:23 -0500

    Updated nuspec

--- IllyumL2T.Core/Properties/AssemblyInfo.cs
+++ IllyumL2T.Core/Properties/AssemblyInfo.cs
@@ -1,5 +1,4 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
@@ -10,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Illyum")]
 [assembly: AssemblyProduct("IllyumL2T")]
-[assembly: AssemblyCopyright("Copyright © 2015 Illyum")]
+[assembly: AssemblyCopyright("Copyright © 2016 Illyum")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,4 +31,4 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.2.*")]
+[assembly: AssemblyVersion("1.3.*")]
