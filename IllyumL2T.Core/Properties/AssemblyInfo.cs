2ee5b643b693ecc61423ebab37c4436b0d12813c
Francisco Javier Banos Lemoine <franl@illyum.com>
2016-07-24 15:27:24 -0500

    Previous commit and push omitted to describe for the Nuget community that now IllyumL2T now supports positional parsing

--- IllyumL2T.Core/Properties/AssemblyInfo.cs
+++ IllyumL2T.Core/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("IllyumL2T")]
-[assembly: AssemblyDescription("A set of classes to parse delimiter-separated values in lines of text and map them into .NET CLR objects")]
+[assembly: AssemblyDescription("A set of classes to parse delimiter-separated or positional values in lines of text and map them into .NET CLR objects")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Illyum")]
 [assembly: AssemblyProduct("IllyumL2T")]
