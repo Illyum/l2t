8da4a2b8d2518ca65cadebed423d37ca2001d6c1
marcodorantes <marcodorantes@hotmail.com>
2018-03-15 20:40:46 -0600

    Added SkypeExport

---
+++ IllyumL2T.Core.UnitTests/Classes for Testing/SkypeLine.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace IllyumL2T.Core.UnitTests
+{
+  public class SkypeLine
+  {
+    public string ConversationId { get; set; }
+    public string ConversationName { get; set; }
+    public string AuthorId { get; set; }
+    public string AuthorName { get; set; }
+    public string HumanTime { get; set; }
+    public string TimestampMs { get; set; }
+    public string ContentXml { get; set; }
+  }
+}
---
+++ IllyumL2T.Core.UnitTests/SkypeExport.cs
@@ -0,0 +1,33 @@
+﻿using System.Linq;
+using System.IO;
+using IllyumL2T.Core.Parse;
+using IllyumL2T.Core.UnitTests;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace IllyumL2T.Core.UnitTests
+{
+  [TestClass]
+  public class SkypeExport
+  {
+    [TestMethod]
+    public void Skype_conversation()
+    {
+      // Arrange
+      var skype_export_file = @"filex.csv";
+      using (var reader = new StreamReader(skype_export_file))
+      {
+        var fileParser = new DelimiterSeparatedValuesFileParser<SkypeLine>();
+
+        // Act
+        var parseResults = fileParser.Read(reader, delimiter: ',', includeHeaders: false);
+        var itr=parseResults.GetEnumerator();
+        Assert.IsTrue(itr.MoveNext());
+        Assert.IsNotNull(itr.Current.Instance);
+        Assert.AreEqual<string>("TimestampMs", itr.Current.Instance.TimestampMs);
+
+        Assert.IsTrue(itr.MoveNext());
+        Assert.IsNotNull(itr.Current.Instance);
+      }
+    }
+  }
+}
